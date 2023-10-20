33564391e2851650d9887d5d7f7f3a6838888a45
Ousama Abd Rabouh <ossama.abd.sy@gmail.com>
2023-10-20 22:05:22 +0300

    second

--- dist/js/about.355801c7.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(a,s,t){"use strict";t.r(s);var n=function(){var a=this,s=a._self._c;return s("div",{staticClass:"about"},[s("h1",[a._v("This is an about page")]),s("h2",[a._v(a._s(a.haha))])])},o=[],e={name:"App",components:{},props:["items"],data:()=>({drawer:null,haha:(void 0).$props["results"]})},p=e,u=t("2877"),i=Object(u["a"])(p,n,o,!1,null,null,null);s["default"]=i.exports}}]);
-//# sourceMappingURL=about.355801c7.js.map
